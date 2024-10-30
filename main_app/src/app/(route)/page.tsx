e17abafa7bb7cb81aa2b7c6eca930059bdaf9937
d-nakajima <nakajima.daishi0329@gmail.com>
2024-10-30 20:33:16 +0800

    react-virtuoso導入

--- main_app/src/app/(route)/page.tsx
+++ main_app/src/app/(route)/page.tsx
@@ -2,9 +2,9 @@ import ChatRoom from "../_components/Chat/ChatRoom";
 
 export default function Home() {
   return (
-    <div className="m-auto grid grid-rows-[20px_1fr_20px] items-center justify-items-center  font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <ChatRoom messages={[]} />
+    <div className="m-auto font-[family-name:var(--font-geist-sans)]">
+      <main className="row-start-2">
+        <ChatRoom />
       </main>
     </div>
   );
--- main_app/src/app/_components/Chat/ChatRoom.tsx
+++ main_app/src/app/_components/Chat/ChatRoom.tsx
@@ -1,47 +1,61 @@
 // components/ChatRoom.tsx
 "use client";
 
-import React, { useState } from "react";
+import React, { memo, useState } from "react";
 import { format } from "date-fns";
 import { ja } from "date-fns/locale"; // 変更
 
 import Message from "./Message";
 import DateTag from "./DateTag";
 import { getMessages } from "@/app/lib/storage";
+import { Virtuoso } from "react-virtuoso";
 
-type Props = {
-  messages: {
-    text: string;
-    date: string;
-    sender: "incoming" | "outgoing";
-  }[];
-};
-
-export default function ChatRoom(props: Props) {
-  console.log(props);
+export default function ChatRoom() {
   const [searchTerm, setSearchTerm] = useState("");
-
-  const isSameDate = (date1: Date, date2: Date | undefined) => {
-    if (!date2) return false;
-    return format(date1, "yyyy/MM/dd") === format(date2, "yyyy/MM/dd");
-  };
-  const isSameYear = (date1: Date, date2: Date | undefined) => {
-    if (!date2) return false;
-    return format(date1, "yyyy") === format(date2, "yyyy");
-  };
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
   };
 
-  const filteredMessages = getMessages("大志")
-    .filter((message) =>
-      message.text?.toLowerCase().includes(searchTerm.toLowerCase())
-    )
-    .slice(0, 100);
+  const filteredMessages = getMessages("大志").filter((message) =>
+    message.text?.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const ContentMemo = memo(function ContentMemo({ index }: { index: number }) {
+    const message = filteredMessages[index];
+    const prevMessage = filteredMessages[index - 1];
+
+    const isSameDate = prevMessage
+      ? format(message.date, "yyyy/MM/dd") ===
+        format(prevMessage.date, "yyyy/MM/dd")
+      : false;
+    const isSameYear = prevMessage
+      ? format(message.date, "yyyy") === format(prevMessage.date, "yyyy")
+      : false;
+    const isLastMessage = index === filteredMessages.length - 1;
+
+    return (
+      <div className={`m-2 ${isLastMessage ? "pb-4" : ""}`}>
+        {!isSameDate && (
+          <DateTag
+            date={
+              isSameYear
+                ? format(message.date, "MM/dd(E)", { locale: ja })
+                : format(message.date, "yyyy/MM/dd(E)", {
+                    locale: ja,
+                  })
+            }
+          />
+        )}
+        <Message {...message} />
+      </div>
+    );
+  });
+
+  const Content = (index: number) => <ContentMemo index={index} />;
 
   return (
-    <div className="w-screen overflow-x-hidden flex flex-col h-screen bg-[#92aad5] ">
+    <div className="w-screen flex flex-col h-screen bg-[#92aad5]">
       {/* Header */}
       <div className="h-16 flex items-center justify-between p-4 bg-white shadow sticky top-0 left-0 right-0">
         {/* BackButton */}
@@ -57,24 +71,11 @@ export default function ChatRoom(props: Props) {
       </div>
 
       {/* Messages */}
-      <div className="flex-1 overflow-y-auto p-4 space-y-4">
-        {filteredMessages.map((message, i) => (
-          <div key={i}>
-            {!isSameDate(message.date, filteredMessages[i - 1]?.date) && (
-              <DateTag
-                date={
-                  isSameYear(message.date, filteredMessages[i - 1]?.date)
-                    ? format(message.date, "MM/dd(E)", { locale: ja })
-                    : format(message.date, "yyyy/MM/dd(E)", {
-                        locale: ja,
-                      })
-                }
-              />
-            )}
-            <Message {...message} />
-          </div>
-        ))}
-      </div>
+      <Virtuoso
+        className="w-full max-w-[840px] m-auto"
+        totalCount={filteredMessages.length}
+        itemContent={Content}
+      />
     </div>
   );
 }
